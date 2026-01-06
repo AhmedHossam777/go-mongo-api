ad4b95cf1caf40b1d440a0e87a72cb781b03c644
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-01-06 23:04:14 +0200

    ii

--- middlewares/cors_middleware.go
+++ middlewares/cors_middleware.go
@@ -7,17 +7,17 @@ func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			// Set CORS headers
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set(
-				"Access-Control-Allow-Methods",
-				"GET, POST, PUT, PATCH, DELETE, OPTIONS",
-			)
-			w.Header().Set(
-				"Access-Control-Allow-Headers",
-				"Accept, Authorization, Content-Type, X-Requested-With",
-			)
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			w.Header().Set("Access-Control-Max-Age", "3600")
+			//w.Header().Set("Access-Control-Allow-Origin", "*")
+			//w.Header().Set(
+			//	"Access-Control-Allow-Methods",
+			//	"GET, POST, PUT, PATCH, DELETE, OPTIONS",
+			//)
+			//w.Header().Set(
+			//	"Access-Control-Allow-Headers",
+			//	"Accept, Authorization, Content-Type, X-Requested-With",
+			//)
+			//w.Header().Set("Access-Control-Allow-Credentials", "true")
+			//w.Header().Set("Access-Control-Max-Age", "3600")
 
 			// Handle preflight requests
 			if r.Method == http.MethodOptions {
