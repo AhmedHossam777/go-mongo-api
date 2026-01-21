6d5e2b911e3d79413f3a5f2f7cbeb7fe3b62be28
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-01-21 23:10:34 +0200

    enable cors

--- middlewares/cors_middleware.go
+++ middlewares/cors_middleware.go
@@ -2,22 +2,21 @@ package middlewares
 
 import "net/http"
 
-// CORSMiddleware adds Cross-Origin Resource Sharing headers to responses
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			// Set CORS headers
-			//w.Header().Set("Access-Control-Allow-Origin", "*")
-			//w.Header().Set(
-			//	"Access-Control-Allow-Methods",
-			//	"GET, POST, PUT, PATCH, DELETE, OPTIONS",
-			//)
-			//w.Header().Set(
-			//	"Access-Control-Allow-Headers",
-			//	"Accept, Authorization, Content-Type, X-Requested-With",
-			//)
-			//w.Header().Set("Access-Control-Allow-Credentials", "true")
-			//w.Header().Set("Access-Control-Max-Age", "3600")
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set(
+				"Access-Control-Allow-Methods",
+				"GET, POST, PUT, PATCH, DELETE, OPTIONS",
+			)
+			w.Header().Set(
+				"Access-Control-Allow-Headers",
+				"Accept, Authorization, Content-Type, X-Requested-With",
+			)
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Max-Age", "3600")
 
 			// Handle preflight requests
 			if r.Method == http.MethodOptions {
