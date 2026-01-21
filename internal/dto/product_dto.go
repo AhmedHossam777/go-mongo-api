24b2607009d898d469046724b9209c2377786cd1
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-01-21 23:20:43 +0200

    add product dto and model

---
+++ internal/dto/product_dto.go
@@ -0,0 +1,15 @@
+package dto
+
+type CreateProduct struct {
+	Title       string `json:"title" validate:"required,min=2,max=100"`
+	ProductCode string `json:"product_code" validate:"required"`
+	Price       int    `json:"price" validate:"required,gt=0"`
+	Description string `json:"description" validate:"required,min=5"`
+}
+
+type UpdateProduct struct {
+	Title       string `json:"title" validate:"omitempty,min=2,max=100"`
+	ProductCode string `json:"product_code" validate:"omitempty"`
+	Price       int    `json:"price" validate:"omitempty,gt=0"`
+	Description string `json:"description" validate:"omitempty,min=5"`
+}
